9f7a5050742f2db1ce55cb61956c453171862b59
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2022-05-29 02:34:15 +0200

    new recipe endpoint

--- main.go
+++ main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"net/http"
 	"time"
 )
 
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
 func main() {
 	router := gin.Default()
+	router.POST("/recipes", NewRecipeHandler)
 	router.Run()
 }
 
 type Recipe struct {
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
+	ID           string    `json:"id"`
+	Name         string    `json:"name" json:"name,omitempty"`
+	Tags         []string  `json:"tags" json:"tags,omitempty"`
+	Ingredients  []string  `json:"ingredients" json:"ingredients,omitempty"`
+	Instructions []string  `json:"instructions" json:"instructions,omitempty"`
+	PublishedAt  time.Time `json:"publishedAt" json:"publishedAt"`
+}
+
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+
 }
